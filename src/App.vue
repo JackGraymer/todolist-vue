17eae3a31ff36df2eede2fc6796df8db3e94d9a7
Alvaro Cervan <jackgraymer@gmail.com>
2023-07-14 01:51:03 +0200

    Adds todo functionality with Vue and localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,141 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from 'vue';
+
+const todos = ref([]);
+const name = ref('');
+
+const input_content = ref('');
+const priority = ref('Pending');
+
+const todos_asc = computed(() =>
+	todos.value.sort((a, b) => {
+		return b.createdAt - a.createdAt;
+	})
+);
+
+const removeTodo = (todo) => {
+	const index = todos.value.indexOf(todo);
+	todos.value.splice(index, 1);
+};
+
+watch(
+	todos,
+	(newval) => {
+		localStorage.setItem('todos', JSON.stringify(newval));
+	},
+	{ deep: true }
+);
+
+watch(name, (newval) => {
+	localStorage.setItem('name', newval);
+});
+
+onMounted(() => {
+	if (localStorage.name !== 'undefined' && localStorage.name !== '') {
+		name.value = localStorage.getItem('name');
+	} else {
+		name.value = '';
+	}
+	if (localStorage.todos == null) {
+		return;
+	}
+	todos.value = JSON.parse(localStorage.getItem('todos'));
+});
+
+const addTodo = () => {
+	if (input_content.value === '') {
+		return;
+	}
+
+	const todo = {
+		id: self.crypto.randomUUID(),
+		content: input_content.value,
+		priority: priority.value,
+		done: false,
+		createdAt: new Date().getTime(),
+	};
+
+	todos.value.push(todo);
+	input_content.value = '';
+	priority.value = 'Pending';
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<main class="app">
+		<section class="greeting">
+			<h2 class="title">
+				What's up
+				<input
+					type="text"
+					class="title"
+					placeholder="Your name"
+					v-model="name" />
+				?
+			</h2>
+		</section>
+		<section class="create-todo">
+			<h3>CREATE A TODO</h3>
+
+			<form v-on:submit.prevent="addTodo">
+				<h4>Your pending Tasks:</h4>
+				<input
+					type="text"
+					placeholder="Enter a todo"
+					v-model="input_content"
+					required
+					v-on:keypress.Enter="addTodo" />
+
+				<div class="options">
+					<label>
+						<input
+							type="radio"
+							name="priority"
+							value="Pending"
+							v-model="priority" />
+						<div>Pending</div>
+					</label>
+					<label>
+						<input
+							type="radio"
+							name="priority"
+							value="Urgent"
+							v-model="priority" />
+						<div>Urgent</div>
+					</label>
+				</div>
+				<button type="submit" value="Add Todo">
+					Add Todo <br />
+					or press <code>Enter</code>
+				</button>
+			</form>
+		</section>
+
+		<section class="todo-list">
+			<h3>To Do List</h3>
+			<div class="list">
+				<div
+					v-for="todo in todos_asc"
+					:class="`todo-item ${todo.done && 'done'}`">
+					<label>
+						<input
+							type="checkbox"
+							v-model="todo.done"
+							:class="`checkbox ${todo.priority}`" />
+						<div class="todo-content">
+							<input type="text" v-model="todo.content" /> {{ todo.content }}
+						</div>
+						<div class="action">
+							<button class="delete" v-on:click="removeTodo(todo)">
+								Delete
+							</button>
+						</div>
+					</label>
+				</div>
+			</div>
+			{{ todos_asc }}
+		</section>
+	</main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
